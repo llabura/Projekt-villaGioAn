fdf512e7e7c7309cb7c3b63bbf8ea1dc5c80a97b
llabura <lucija.labura@gmail.com>
2020-11-29 19:33:50 +0100

    Updated link to repository

--- ntpws php/dbconn.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-	# Connect to MySQL database
-	$MySQL = mysqli_connect("localhost","root","","villa_gioan") or die('Error connecting to MySQL server.');
--- ntpws php/functions.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	function pickerDateToMysql($pickerDate){
-		$date = DateTime::createFromFormat('Y-m-d H:i:s', $pickerDate);
-		return $date->format('d. m. Y H:i:s');
-	}  
-?>
